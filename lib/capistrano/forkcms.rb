6c58c867cced60745c76234f3410bfd75123915f
Tijs Verkoyen <tijs@verkoyen.eu>
2017-03-22 10:47:28 +0100

    WIP Trying to get Composer to run correctly

--- lib/capistrano/forkcms.rb
+++ lib/capistrano/forkcms.rb
@@ -1,7 +1,5 @@
 require "capistrano/composer"
 
-load File.expand_path('../tasks/fork.rake', __FILE__)
-
 namespace :load do
   task :defaults do
     load "capistrano/forkcms/defaults.rb"
--- lib/capistrano/forkcms/defaults.rb
+++ lib/capistrano/forkcms/defaults.rb
@@ -5,3 +5,13 @@ set :linked_files, []
 set :linked_files, -> { ["app/config/parameters.yml"] }
 set :linked_dirs, []
 set :linked_dirs, -> { ["app/logs", "app/sessions", "src/Frontend/Files"] }
+
+#
+# Capistrano/Composer defaults
+#
+puts deploy_to
+
+SSHKit.config.command_map[:composer] = "php #{deploy_to}/shared/composer.phar"
+
+set :composer_working_dir, -> { fetch(:release_path) }
+set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader --no-scripts'
