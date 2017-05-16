63aa2fd6d0fc0ad83bc5b0174ebfeaccf720c7f3
Tijs Verkoyen <tijs@verkoyen.eu>
2017-05-16 13:46:08 +0200

    Make sure composer is installed

--- lib/capistrano/forkcms/defaults.rb
+++ lib/capistrano/forkcms/defaults.rb
@@ -15,3 +15,7 @@ SSHKit.config.command_map[:composer] = "php #{deploy_to}/shared/composer.phar"
 
 set :composer_working_dir, -> { fetch(:release_path) }
 set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader --no-scripts'
+
+namespace :deploy do
+  after :starting, 'composer:install_executable'
+end
